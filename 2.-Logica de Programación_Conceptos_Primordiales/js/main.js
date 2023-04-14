633f6484351ff06612519dd5f3368c6e04832f38
Mauricio Castillo Zavala <angel.castillo.zavala@estudiante.uacm.edu.mx>
2023-04-14 15:31:19 -0600

    se creo los elementos de futbol

--- 2.-Logica de Programación_Conceptos_Primordiales/js/main.js
+++ 2.-Logica de Programación_Conceptos_Primordiales/js/main.js
@@ -6,3 +6,10 @@ function imprimir(params) {
     document.write(params);
     saltarLinea( );    
 }
+
+var victorias = parseInt(prompt ("Informe de la cantidad de Victorias: "));
+var empates  = parseInt(prompt ("Informe de la cantidad de empates: "));
+
+var puntosTotales = (victorias * 3) + empates;
+
+imprimir ("El total de los puntos del equipo es: " + puntosTotales);
