7bdf1c19238c96d33573eec52d3e8129a927c659
Mauricio Castillo Zavala <angel.castillo.zavala@estudiante.uacm.edu.mx>
2023-04-14 15:37:28 -0600

    se agrego condicionales para los puntos totales

--- 2.-Logica de Programación_Conceptos_Primordiales/js/main.js
+++ 2.-Logica de Programación_Conceptos_Primordiales/js/main.js
@@ -13,3 +13,11 @@ var empates  = parseInt(prompt ("Informe de la cantidad de empates: "));
 var puntosTotales = (victorias * 3) + empates;
 
 imprimir ("El total de los puntos del equipo es: " + puntosTotales);
+
+if (puntosTotales > 28) {
+    imprimir ("El equipo está mejor que el año pasado");    
+} else if (puntosTotales < 28) {
+    imprimir ("El equipo está peor que el año pasado");    
+}else if (puntosTotales == 28){
+    imprimir ("El equipo está igual que el año pasado");
+}
