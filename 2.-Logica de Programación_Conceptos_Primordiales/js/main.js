d9a858c765c8289913f100a3df260bf5c2a9c0a1
Mauricio Castillo Zavala <angel.castillo.zavala@estudiante.uacm.edu.mx>
2023-04-14 18:38:45 -0600

    se creo las funciones de indices de imc

--- 2.-Logica de Programación_Conceptos_Primordiales/js/main.js
+++ 2.-Logica de Programación_Conceptos_Primordiales/js/main.js
@@ -7,6 +7,42 @@ function imprimir(params) {
     saltarLinea( );    
 }
 
+
+
+
+function imcFun(peso, altura) {
+    
+    return peso/ (Math.pow(altura, 2));
+}
+
+function tipoObesidad(imc, nom) {
+    if (imc < 18.5) {
+        return nom + " Su imc es:"+ imc + ", Usted tiene Insuficiencia ponderal";
+    } else if (imc >= 18.5 && imc < 25) {
+        return nom + " Su imc es:"+ imc + ", Usted tiene un Intervalo Normal";
+    } else if (imc >= 25 && imc < 30) {
+        return nom + " Su imc es:"+ imc + ", Usted tiene Sobrepeso (Preobesidad)";
+    } else if (imc >= 30) {
+        if (imc >= 30 && imc < 35) {
+            return nom + " Su imc es:"+ imc + ", Usted tiene Obesidad de Clase I";
+        } else if (imc >= 35 && imc < 40) {
+            return nom + " Su imc es:"+ imc + ", Usted tiene Obesidad de Clase II";
+        } else if (imc >= 40) {
+            return nom + "  Su imc es:"+ imc + ", Usted tiene Obesidad  de Clase III";
+        }
+    }
+}
+
+
+var nombre = prompt ("Nombre: ");
+var peso = parseFloat(prompt(nombre + ", Dame tu peso: "))
+var altura = parseFloat (prompt (nombre + ", Dame tu altura: "));
+
+imcPersona = imcFun(peso, altura);
+var prueba = tipoObesidad((imcPersona), nombre);
+imprimir (prueba);
+
+
 var victorias = parseInt(prompt ("Informe de la cantidad de Victorias: "));
 var empates  = parseInt(prompt ("Informe de la cantidad de empates: "));
 
@@ -21,3 +57,5 @@ if (puntosTotales > 28) {
 }else if (puntosTotales == 28){
     imprimir ("El equipo está igual que el año pasado");
 }
+
+
