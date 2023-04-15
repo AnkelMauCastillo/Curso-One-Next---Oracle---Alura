fc4f8060d1927aae0efad9cf086e8f02f634b467
Mauricio Castillo Zavala <angel.castillo.zavala@estudiante.uacm.edu.mx>
2023-04-15 00:17:57 -0600

    se agrego estrella y login

--- 2.-Logica de Programación_Conceptos_Primordiales/js/estrella.js
+++ 2.-Logica de Programación_Conceptos_Primordiales/js/estrella.js
@@ -6,3 +6,13 @@ function imprimir(params) {
     document.write("<h4>"+ params + "</h4>");
     //saltarLinea( );    
 }
+var estrella = '*';
+for (let i = 1; i <=3 ; i++) {    
+   for (let j = 1; j <= 10; j++) {
+        document.write( estrella );        
+    
+   }
+   saltarLinea();
+}
+
+imprimir ("fin");
